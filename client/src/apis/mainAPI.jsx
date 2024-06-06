175afe734638cd628ccef2b4327f4b920142535e
bombr <47834716+bombr90@users.noreply.github.com>
2024-06-05 23:29:50 -0700

    Changed FE API calls to external domain

--- client/src/apis/mainAPI.jsx
+++ client/src/apis/mainAPI.jsx
@@ -1,8 +1,12 @@
+// Change this to the domain and port you will be hosting your backend service unless its on the same machine
+// const domain = "localhost:5000";
+const domain = "https://top-odin-book.onrender.com:5000";
+
 export const mainAPI = {
   userIndex: async (page, limit) => {
     //  Get/traverse user index
     const response = await fetch(
-      `http://localhost:5000/api/v1/user/index?page=${page}&limit=${limit}`,
+      `http://${domain}/api/v1/user/index?page=${page}&limit=${limit}`,
       {
         method: "get",
         mode: "cors",
@@ -14,7 +18,7 @@ export const mainAPI = {
   postIndex: async (page, limit) => {
     // Get/traverse use feed
     const response = await fetch(
-      `http://localhost:5000/api/v1/user/posts/index?page=${page}&limit=${limit}`,
+      `http://${domain}/api/v1/user/posts/index?page=${page}&limit=${limit}`,
       {
         method: "get",
         mode: "cors",
@@ -25,18 +29,15 @@ export const mainAPI = {
   },
   postDetails: async (id) => {
     //  Get individual post and comments thread
-    const response = await fetch(
-      `http://localhost:5000/api/v1/user/post/${id}`,
-      {
-        method: "get",
-        mode: "cors",
-        credentials: "include",
-      }
-    );
+    const response = await fetch(`http://${domain}/api/v1/user/post/${id}`, {
+      method: "get",
+      mode: "cors",
+      credentials: "include",
+    });
     return await response.json();
   },
   postPost: async (data) => {
-    const response = await fetch(`http://localhost:5000/api/v1/user/post`, {
+    const response = await fetch(`http://${domain}/api/v1/user/post`, {
       method: "post",
       mode: "cors",
       credentials: "include",
@@ -49,24 +50,21 @@ export const mainAPI = {
   },
   postComment: async (data) => {
     const { id } = data;
-    const response = await fetch(
-      `http://localhost:5000/api/v1/user/post/${id}`,
-      {
-        method: "post",
-        mode: "cors",
-        credentials: "include",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      }
-    );
+    const response = await fetch(`http://${domain}/api/v1/user/post/${id}`, {
+      method: "post",
+      mode: "cors",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
     return await response.json();
   },
   likePost: async (data) => {
     const { id } = data;
     const response = await fetch(
-      `http://localhost:5000/api/v1/user/post/${id}/like`,
+      `http://${domain}/api/v1/user/post/${id}/like`,
       {
         method: "put",
         mode: "cors",
@@ -77,31 +75,25 @@ export const mainAPI = {
   },
   deletePost: async (data) => {
     const { id } = data;
-    const response = await fetch(
-      `http://localhost:5000/api/v1/user/post/${id}`,
-      {
-        method: "delete",
-        mode: "cors",
-        credentials: "include",
-      }
-    );
+    const response = await fetch(`http://${domain}/api/v1/user/post/${id}`, {
+      method: "delete",
+      mode: "cors",
+      credentials: "include",
+    });
     return await response.json();
   },
   deleteComment: async (data) => {
     const { id } = data;
-    const response = await fetch(
-      `http://localhost:5000/api/v1/user/comment/${id}`,
-      {
-        method: "delete",
-        mode: "cors",
-        credentials: "include",
-      }
-    );
+    const response = await fetch(`http://${domain}/api/v1/user/comment/${id}`, {
+      method: "delete",
+      mode: "cors",
+      credentials: "include",
+    });
     return await response.json();
   },
   postFriendRequest: async (data) => {
     const response = await fetch(
-      `http://localhost:5000/api/v1/user/friendsrequest`,
+      `http://${domain}/api/v1/user/friendsrequest`,
       {
         method: "post",
         mode: "cors",
@@ -116,7 +108,7 @@ export const mainAPI = {
   },
   deleteFriendRequest: async (data) => {
     const response = await fetch(
-      `http://localhost:5000/api/v1/user/friendsrequest`,
+      `http://${domain}/api/v1/user/friendsrequest`,
       {
         method: "delete",
         mode: "cors",
@@ -131,7 +123,7 @@ export const mainAPI = {
   },
   putFriendRequest: async (data) => {
     const response = await fetch(
-      `http://localhost:5000/api/v1/user/friendsrequest`,
+      `http://${domain}/api/v1/user/friendsrequest`,
       {
         method: "put",
         mode: "cors",
@@ -145,7 +137,7 @@ export const mainAPI = {
     return await response.json();
   },
   deleteFriend: async (data) => {
-    const response = await fetch(`http://localhost:5000/api/v1/user/friends`, {
+    const response = await fetch(`http://${domain}/api/v1/user/friends`, {
       method: "delete",
       mode: "cors",
       credentials: "include",
@@ -158,18 +150,15 @@ export const mainAPI = {
   },
   getUserProfile: async (data) => {
     const { id } = data;
-    const response = await fetch(
-      `http://localhost:5000/api/v1/user/${id}/profile`,
-      {
-        method: "get",
-        mode: "cors",
-        credentials: "include",
-      }
-    );
+    const response = await fetch(`http://${domain}/api/v1/user/${id}/profile`, {
+      method: "get",
+      mode: "cors",
+      credentials: "include",
+    });
     return await response.json();
   },
   getUserData: async () => {
-    const response = await fetch(`http://localhost:5000/api/v1/user/data`, {
+    const response = await fetch(`http://${domain}/api/v1/user/data`, {
       method: "get",
       mode: "cors",
       credentials: "include",
@@ -177,11 +166,11 @@ export const mainAPI = {
     return await response.json();
   },
   getIsAuth: async () => {
-     const response = await fetch(`http://localhost:5000/api/v1/`, {
-       method: "get",
-       mode: "cors",
-       credentials: "include",
-     });
-     return await response.json();
-  }
+    const response = await fetch(`http://${domain}/api/v1/`, {
+      method: "get",
+      mode: "cors",
+      credentials: "include",
+    });
+    return await response.json();
+  },
 };
